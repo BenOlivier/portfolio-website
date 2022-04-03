fe5e417dc0093070f09c512e35526b0da3c69bf4
BenOlivier <dbeolivier@gmail.com>
2022-04-03 20:56:28 +0100

    Implement post processing

--- src/Experience/Renderer.js
+++ src/Experience/Renderer.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 import Experience from './Experience.js'
 
 export default class Renderer
@@ -11,34 +14,49 @@ export default class Renderer
         this.scene = this.experience.scene
         this.camera = this.experience.camera
 
-        this.setInstance()
+        this.setRenderer()
     }
 
-    setInstance()
+    setRenderer()
     {
-        this.instance = new THREE.WebGLRenderer({
+        this.renderer = new THREE.WebGLRenderer({
             canvas: this.canvas,
             antialias: true,
+            sortObjects: false,
             alpha: true
         })
-        this.instance.physicallyCorrectLights = true
-        this.instance.outputEncoding = THREE.sRGBEncoding
-        this.instance.toneMapping = THREE.CineonToneMapping
-        this.instance.toneMappingExposure = 1.75
-        this.instance.shadowMap.enabled = true
-        this.instance.shadowMap.type = THREE.PCFSoftShadowMap
-        this.instance.setSize(this.sizes.width, this.sizes.height)
-        this.instance.setPixelRatio(Math.min(this.sizes.pixelRatio, 2))
+        this.renderer.physicallyCorrectLights = true
+        this.renderer.outputEncoding = THREE.sRGBEncoding
+        this.renderer.toneMapping = THREE.CineonToneMapping
+        this.renderer.toneMappingExposure = 1.75
+        this.renderer.shadowMap.enabled = true
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
+        this.renderer.setSize(this.sizes.width, this.sizes.height)
+        this.renderer.setPixelRatio(Math.min(this.sizes.pixelRatio, 2))
+
+        this.renderTarget = new THREE.WebGLRenderTarget()
+        this.effectComposer = new EffectComposer(this.renderer, this.renderTarget)
+        this.effectComposer.setSize(this.sizes.width, this.sizes.height)
+        this.effectComposer.setPixelRatio(Math.min(this.sizes.pixelRatio, 2))
+
+        this.renderScene = new RenderPass(this.scene, this.camera.instance)
+        this.effectComposer.addPass(this.renderScene)
+
+        this.bloomPass = new UnrealBloomPass()
+        this.bloomPass.threshold = 1;
+        this.bloomPass.strength = 10;
+
+        // this.effectComposer.addPass(this.bloomPass)
     }
 
     resize()
     {
-        this.instance.setSize(this.sizes.width, this.sizes.height)
-        this.instance.setPixelRatio(Math.min(this.sizes.pixelRatio, 2))
+        this.renderer.setSize(this.sizes.width, this.sizes.height)
+        this.renderer.setPixelRatio(Math.min(this.sizes.pixelRatio, 2))
     }
 
     update()
     {
-        this.instance.render(this.scene, this.camera.instance)
+        this.effectComposer.render()
     }
 }

d78ab0bac0702bfe14e1365c0c287acc9e6617c7
BenOlivier <dbeolivier@gmail.com>
2022-04-29 22:53:17 +0100

    New HDRI

--- src/Experience/Renderer.js
+++ src/Experience/Renderer.js
@@ -33,7 +33,7 @@ export default class Renderer
         this.renderer.physicallyCorrectLights = true
         this.renderer.outputEncoding = THREE.sRGBEncoding
         this.renderer.toneMapping = THREE.CineonToneMapping
-        this.renderer.toneMappingExposure = 0.8
+        this.renderer.toneMappingExposure = 1
         this.renderer.shadowMap.enabled = true
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
         this.renderer.setSize(this.sizes.width, this.sizes.height)

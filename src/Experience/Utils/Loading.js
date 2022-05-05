cffeec393a9ae2f433d4410e8ba668b3dc962d97
BenOlivier <dbeolivier@gmail.com>
2022-05-05 10:41:08 +0100

    Update nav bar

--- src/Experience/Utils/Loading.js
+++ src/Experience/Utils/Loading.js
@@ -10,31 +10,10 @@ export default class Loading
         this.sizes = this.experience.sizes
         this.time = this.experience.time
         this.scene = this.experience.scene
-
-        // Events
-        this.sizes.on('resize', () =>
-        {
-            this.resize()
-        })
-        this.time.on('tick', () =>
-        {
-            // if(this.loadingBar.parent == this.scene) this.updateLoadingBar()
-        })
-        
-        // this.setLoadingBar()
     }
 
     initiateLoadedSequence()
     {
-        // // Fade out loading bar
-        // setTimeout(() => {
-        //     this.fadeLoadingBar()
-        // }, 250)
-        // // Destroy loading bar and overlay
-        // setTimeout(() => {
-        //     this.destroy()
-        // }, 2200)
-
         // Animate in hello
         this.model = this.experience.object.model
         gsap.to(this.model.position, {
@@ -53,82 +32,4 @@ export default class Loading
             value: 0.5
         })
     }
-
-    // setLoadingBar()
-    // {
-    //     this.loadingBarWidth = 0.5
-
-    //     if(this.sizes.width > 1000)
-    //     {
-    //         this.loadingBarWidth = 300 / this.sizes.width
-    //     }
-        
-    //     this.loadingBarGeometry = new THREE.PlaneGeometry(this.loadingBarWidth,
-    //         this.sizes.height * 0.00001, 1, 1)
-    //     this.loadingBarMaterial = new THREE.ShaderMaterial({
-    //         transparent: true,
-    //         uniforms:
-    //         {
-    //             uProgress: { value: 0.0 },
-    //             uAlpha: { value: 1.0 },
-    //             uResolution: { value: new THREE.Vector2(this.sizes.width, this.sizes.height) }
-    //         },
-    //         vertexShader: `
-    //             varying vec2 vUv;
-
-    //             void main()
-    //             {
-    //                 gl_Position = vec4(position, 1.0);
-    //                 vUv = uv;
-    //             }
-    //         `,
-    //         fragmentShader: `
-    //             uniform float uProgress;
-    //             uniform float uAlpha;
-    //             varying vec2 vUv;
-
-    //             void main()
-    //             {
-    //                 float fragColor = step(vUv.x, uProgress);
-    //                 gl_FragColor = vec4(vec3(fragColor), uAlpha);
-    //             }
-    //         `
-    //     })
-
-    //     this.loadingBar = new THREE.Mesh(this.loadingBarGeometry, this.loadingBarMaterial)
-    //     this.loadingBar.renderOrder = 999
-    //     this.loadingBar.position.set(0, 2, 0)
-    //     this.scene.add(this.loadingBar)
-    // }
-
-    // updateLoadingBar()
-    // {
-    //     if(this.experience.resources.progressRatio == 0)
-    //     {
-    //         this.loadingBarMaterial.uniforms.uProgress.value += 0.002
-    //     }
-    //     else
-    //     {
-    //         this.loadingBarMaterial.uniforms.uProgress.value +=
-    //             this.experience.resources.progressRatio * 0.05
-    //     }
-    // }
-
-    // resize()
-    // {
-    //     this.loadingBarMaterial.uniforms.uResolution.value.x
-    //         = this.sizes.width
-    //     this.loadingBarMaterial.uniforms.uResolution.value.y
-    //         = this.sizes.height
-    // }
-
-    // destroy()
-    // {
-    //     this.scene.remove(this.loadingBar)
-    //     this.scene.remove(this.overlay)
-    //     this.overlayGeometry.dispose()
-    //     this.loadingBarGeometry.dispose()
-    //     this.overlayMaterial.dispose()
-    //     this.loadingBarMaterial.dispose()
-    // }
 }

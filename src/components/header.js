500977b6ac1c1aed2aaaafbdd0e719d8d4f80af1
BenOlivier <dbeolivier@gmail.com>
2022-06-28 17:51:01 +0100

    Implement hamburger nav menu

--- src/components/header.js
+++ src/components/header.js
@@ -10,11 +10,16 @@ class Header extends HTMLElement
         this.innerHTML = `
             <header>
                 <h1><a href="/index.html">Ben Olivier</a></h1>
-                <nav>
+                <nav id="nav">
                     <a href="/about.html">ABOUT</a>
                     <a href="">WORK</a>
                     <a href="/contact.html">CONTACT</a>
                 </nav>
+                <button onclick="this.classList.toggle('collapsed');document.getElementById('nav').classList.toggle('expanded')">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
             </header>
         `
     }

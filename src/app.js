364cf4252c336507fee3a2a0a5b0eef7a433960d
BenOlivier <dbeolivier@gmail.com>
2022-06-29 13:26:31 +0100

    Only instantiate hello scene on homepage

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import './style.css'
 import './css/about.css'
 import './css/contact.css'
 
-// import Experience from './three/experience.js'
+import Experience from './three/experience.js'
 
-// const experience = new Experience(document.querySelector('canvas.webgl'))
+if(document.body.classList.contains('index'))
+{
+    const experience = new Experience(document.querySelector('canvas.webgl'))
+}
--- src/hello.js
+++
@@ -1,5 +0,0 @@
-// import { Experience } from './three/experience.js'
-
-// const experience = new Experience(document.querySelector('canvas.webgl'))
-
-// console.log('yo')

f3bb64465d3cd161c17464cd777b3ac1933c654d
BenOlivier <dbeolivier@gmail.com>
2022-07-03 01:42:20 +0100

    Implement work anchors script

---
+++ src/js/work-anchors.js
@@ -0,0 +1,39 @@
+// if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+// {
+    
+// }
+
+const anchors = {
+    lithoAnchor: document.getElementById('litho-anchor'),
+    dioramaAnchor: document.getElementById('diorama-anchor'),
+    threeDAnchor: document.getElementById('3d-anchor'),
+    otherAnchor: document.getElementById('other-anchor')
+}
+
+Object.values(anchors).forEach(val => {
+    SetEventListeners(val)
+})
+
+function SetEventListeners(element)
+{
+    element.onclick = function()
+    {
+        if(!element.classList.contains('clickable'))
+        {
+            Object.values(anchors).forEach(val => {
+                val.classList.remove('clickable')
+            })
+            element.classList.add('clickable')
+            return false
+        }
+    }
+}
+
+document.addEventListener('click', (e) =>
+{
+    if (!e.target.classList.contains('image-container')) {
+        Object.values(anchors).forEach(val => {
+            val.classList.remove('clickable')
+        })
+    }
+})

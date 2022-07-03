02d06f62841cb0fff59db79d5e707cf01394cfdb
BenOlivier <dbeolivier@gmail.com>
2022-07-03 19:51:09 +0100

    Try make background script work on mobile

--- src/js/html-background.js
+++ src/js/html-background.js
@@ -1,19 +1,36 @@
-let isCeiling = true
+let scrollingUp = true
 let lastScrollPos = 0
 
-window.addEventListener('scroll', (e) =>
+// window.addEventListener('scroll', (e) =>
+// {
+//     if (lastScrollPos > window.scrollY && !isCeiling)
+//     {
+//         document.documentElement.style.background =
+//             getComputedStyle(document.documentElement).getPropertyValue('--bg-white')
+//         isCeiling = true
+//     }
+//     else if (lastScrollPos < window.scrollY && isCeiling)
+//     {
+//         document.documentElement.style.background = '#333333'
+//         isCeiling = false
+//     }
+
+//     lastScrollPos = window.scrollY
+// })
+
+window.onscroll = () =>
 {
-    if (lastScrollPos > window.scrollY && !isCeiling)
+    if (lastScrollPos > window.scrollY && !scrollingUp)
     {
         document.documentElement.style.background =
             getComputedStyle(document.documentElement).getPropertyValue('--bg-white')
-        isCeiling = true
+            scrollingUp = true
     }
-    else if (lastScrollPos < window.scrollY && isCeiling)
+    else if (lastScrollPos < window.scrollY && scrollingUp)
     {
         document.documentElement.style.background = '#333333'
-        isCeiling = false
+        scrollingUp = false
     }
 
     lastScrollPos = window.scrollY
-})
+}

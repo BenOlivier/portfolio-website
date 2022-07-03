781ea93be67bf414bdb170ef577bbf92ce4234f1
BenOlivier <dbeolivier@gmail.com>
2022-07-03 11:45:28 +0100

    Add background wrapper to all pages

--- src/js/work-anchors.js
+++ src/js/work-anchors.js
@@ -1,45 +1,43 @@
-// if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
-// {
-    
-// }
-
-const anchors = {
-    lithoAnchor: document.getElementById('litho-anchor'),
-    dioramaAnchor: document.getElementById('diorama-anchor'),
-    threeDAnchor: document.getElementById('3d-anchor'),
-    otherAnchor: document.getElementById('other-anchor')
-}
-
-Object.values(anchors).forEach(val => {
-    SetEventListeners(val)
-})
-
-function SetEventListeners(element)
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
 {
-    element.onclick = function()
+    const anchors = {
+        lithoAnchor: document.getElementById('litho-anchor'),
+        dioramaAnchor: document.getElementById('diorama-anchor'),
+        threeDAnchor: document.getElementById('3d-anchor'),
+        otherAnchor: document.getElementById('other-anchor')
+    }
+    
+    Object.values(anchors).forEach(val => {
+        SetEventListeners(val)
+    })
+    
+    function SetEventListeners(element)
     {
-        if(!element.classList.contains('clickable'))
+        element.onclick = function()
         {
+            if(!element.classList.contains('clickable'))
+            {
+                Object.values(anchors).forEach(val => {
+                    val.classList.remove('clickable')
+                })
+                element.classList.add('clickable')
+                return false
+            }
+        }
+    }
+    
+    document.addEventListener('click', (e) =>
+    {
+        if (!e.target.classList.contains('image-container')) {
             Object.values(anchors).forEach(val => {
                 val.classList.remove('clickable')
             })
-            element.classList.add('clickable')
-            return false
         }
-    }
-}
-
-document.addEventListener('click', (e) =>
-{
-    if (!e.target.classList.contains('image-container')) {
+    })
+    
+    window.onload = function() {
         Object.values(anchors).forEach(val => {
             val.classList.remove('clickable')
         })
     }
-})
-
-window.onload = function() {
-    Object.values(anchors).forEach(val => {
-        val.classList.remove('clickable')
-    })
 }

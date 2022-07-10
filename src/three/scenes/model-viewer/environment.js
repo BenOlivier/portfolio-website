0ef6dc556ba4958764e18ed14017dd503a3c7b69
BenOlivier <dbeolivier@gmail.com>
2022-07-10 20:26:47 +0100

    Add env map thumbnails

--- src/three/scenes/model-viewer/environment.js
+++ src/three/scenes/model-viewer/environment.js
@@ -5,9 +5,25 @@ export default class Environment
     constructor()
     {
         this.experience = window.experience
+        this.canvas = this.experience.canvas
         this.scene = this.experience.scene
         this.resources = this.experience.resources
         this.renderer = this.experience.renderer
+
+        const settingsButton = document.getElementById('settings-button')
+        const envMapThumbs = document.getElementById('envMapThumbs')
+        const autoshopThumb = envMapThumbs.children[1]
+        const marketThumb = envMapThumbs.children[2]
+        const parkThumb = envMapThumbs.children[3]
+        autoshopThumb.addEventListener('click', (event) => { this.scene.environment = this.envMaps[0] })
+        marketThumb.addEventListener('click', (event) => { this.scene.environment = this.envMaps[1] })
+        parkThumb.addEventListener('click', (event) => { this.scene.environment = this.envMaps[2] })
+
+        this.canvas.addEventListener('click', (event) => {
+            envMapThumbs.classList.remove('visible')
+            settingsButton.classList.remove('hidden')
+        })
+        this.canvas.addEventListener('touchend', (event) => { envMapThumbs.classList.remove('visible') })
 
         this.setEnvironmentMaps()
         this.setFloor()

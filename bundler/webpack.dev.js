c73a4e0da64cd800c2f7d8550539a57a4e8b619a
BenOlivier <dbeolivier@gmail.com>
2026-01-22 11:57:22 +0000

    Update packages and .gitignore

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -38,15 +38,17 @@ module.exports = merge(
                 overlay: true,
                 progress: false
             },
-            onAfterSetupMiddleware: function(devServer)
+            setupMiddlewares: (middlewares, devServer) =>
             {
                 const port = devServer.options.port
                 const https = devServer.options.https ? 's' : ''
                 const localIp = ip.address()
                 const domain1 = `http${https}://${localIp}:${port}`
                 const domain2 = `http${https}://localhost:${port}`
-                
+
                 console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
+
+                return middlewares
             }
         }
     }
